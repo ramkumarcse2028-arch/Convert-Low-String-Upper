1e8f37e5024d0263abd9331e8a6d8883d8954d93
ramkumarcse2028-arch <ramkumarcse2028@gmail.com>
2026-08-27 11:36:17 +0530

    Add files via upload

---
+++ conver_string_to_lower_case.java
@@ -0,0 +1,7 @@
+public class conver_string_to_lower_case {
+    public static void main(String[] args) {
+        String name = " RamKumar ";
+        name = name.toLowerCase();
+        System.out.println(name);
+    }
+}
